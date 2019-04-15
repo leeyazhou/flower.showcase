1c88150a562c0a4f4304f19710ff2a0c1a6a7b14
leeyazhou <lee_yazhou@163.com>
2019-04-15 13:13:19 +0800

    init

---
+++ order-platform/src/main/java/com/ly/train/order/service/order/CreateOrderService.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2019 同程艺龙 (zhihui.li@ly.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ly.train.order.service.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ly.train.flower.common.annotation.FlowerService;
+import com.ly.train.flower.common.service.Service;
+import com.ly.train.flower.common.service.container.ServiceContext;
+import com.ly.train.order.dao.OrderDao;
+import com.ly.train.order.model.OrderDTO;
+
+/**
+ * @author leeyazhou
+ *
+ */
+@FlowerService
+public class CreateOrderService implements Service<OrderDTO, Boolean> {
+
+  @Autowired
+  private OrderDao orderDao;
+
+  @Override
+  public Boolean process(OrderDTO message, ServiceContext context) throws Throwable {
+    return null;
+  }
+
+}
