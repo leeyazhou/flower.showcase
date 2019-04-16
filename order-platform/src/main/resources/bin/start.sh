cb02091da70047cd05bf4329ad8bf6d83379ad62
leeyazhou <lee_yazhou@163.com>
2019-04-16 14:13:58 +0800

    start脚本

--- order-platform/src/main/resources/bin/start.sh
+++ order-platform/src/main/resources/bin/start.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-echo "start flower"
 
-flowerHome=$(cd "../$(dirname "$0")";pwd)
+DATE_CURRENT="`date '+%Y-%m-%d'`"
+flowerHome=$(cd "$(dirname "$0")/../";pwd)
 flowerLibs="${flowerHome}/libs"
+MAIN_CLASS="com.ly.train.flower.container.Bootstrap"
 
-echo "flowerHome : ${flowerHome}"
-echo "flowerLibs : ${flowerLibs}"
+#logger info
+function INFO() {
+	MSG="`date '+%Y-%m-%d %H:%M:%S'` INFO $1"
+	echo -e $MSG
+	#echo -e $MSG >> "$CRPC_LOGS/crpc.$DATE_CURRENT.log"
+}
+
+#logger warn
+function WARN() {
+	MSG="`date '+%Y-%m-%d %H:%M:%S'` WARN $1"
+	echo -e $MSG
+	#echo -e $MSG >> "$CRPC_LOGS/crpc.$DATE_CURRENT.log"
+}
+INFO "start flower"
+INFO "flowerHome : ${flowerHome}"
+INFO "flowerLibs : ${flowerLibs}"
 
 classpath=$classpath
 for jar in ${flowerLibs}/*; do 
 	classpath="$classpath:$jar"
 done
-
-java -classpath  $classpath -Dspring.config.location=conf/spr.xml com.ly.train.flower.container.Bootstrap
+java -classpath  $classpath -Dflower.home=${flowerHome} -Dspring.config.location=conf/spr.xml ${MAIN_CLASS}
 
